4391a48996088e9a4cdaa8b3b29ddc31a90d28a5
kd <krakensden@krakensden.com>
2013-04-26 21:51:22 -0700

    clear & quit

--- minsam.cpp
+++ minsam.cpp
@@ -2,14 +2,37 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System/Clock.hpp>
 
+class Player {
+	sf::CircleShape self;
+	sf::CircleShape sword;
+
+	sf::Keyboard::Key action, wiggle;
+
+	Player()
+	{}
+};
+
 int main() {
 	printf("Hello world\n");
 	sf::RenderWindow window(sf::VideoMode(800, 600), "mini samurai");
+	sf::Clock clock;
 
 	window.clear();
 
 	while (window.isOpen())
 	{
+		sf::Event e;
+		while ( window.pollEvent(e) )
+		{
+				if ( e.type == sf::Event::Closed )
+				{
+					return 0;
+				}
+				if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Escape ) ) 
+				{
+					return 0;
+				}
+		}
 		window.display();
 	}
 }
