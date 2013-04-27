a9a8c450441a7a6590e418f33d44d1c576794d42
kd <krakensden@krakensden.com>
2013-04-27 10:35:24 -0700

    Add swing, awful background image

--- minsam.cpp
+++ minsam.cpp
@@ -1,9 +1,44 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System/Clock.hpp>
+#include <cmath>
+#include <cstdio>
 
 const int width = 800;
 const int height = 800;
 const float stepping = 250.0;
+
+double distance(sf::Vector2f a, sf::Vector2f b)
+{
+	return sqrt(((b.x-a.x)*(b.x-a.x))+((b.y-a.y)*(b.y-a.y)));
+}
+
+double cross(sf::Vector2f a, sf::Vector2f b)
+{
+	return a.x * b.y - b.x * a.y;
+}
+
+double swing(double x)
+{
+	return pow(1.5*x,8);
+}
+
+sf::Vector2f swingFrom(sf::Vector2f src, double percent)
+{
+	sf::Vector2f dst = src, rval;
+
+	// move dst
+	dst.x += 10;
+	dst.y += 50;
+
+	double new_distance =  distance(src, dst)*percent;
+    double direction = atan2(src.y - dst.y, src.x - dst.x);
+
+	rval.x = src.x - cos(direction)*new_distance;
+	rval.y = src.y - sin(direction)*new_distance;
+
+	return rval;
+}
+
 
 class Player {
 	sf::CircleShape self;
@@ -14,6 +49,8 @@ class Player {
 	bool leaping, swinging;
 	bool firstPlayer;
 
+	int64_t elapsedSwingTime;
+
 	public:
 	Player(bool firstPlayer)
 		: self(10)
@@ -21,6 +58,7 @@ class Player {
 		, leaping(false)
 		, swinging(false)
 		, firstPlayer(firstPlayer)
+		, elapsedSwingTime(0)
 	{
 		reset();
 	}
@@ -44,21 +82,43 @@ class Player {
 		if ( firstPlayer )
 		{
 			self.setPosition(width/4,selfYCoord);
-			sword.setPosition(width/4,swordYCoord);
+			//sword.setPosition(width/4,swordYCoord);
 
 			self.setFillColor(sf::Color(250, 218, 221));
+			self.setOutlineColor(sf::Color(0,0,0));
+			self.setOutlineThickness(1);
 			sword.setFillColor(sf::Color(111,111,111));
 		}
 		else 
 		{
 			self.setPosition(3*width/4,selfYCoord);
-			sword.setPosition(3*width/4+pHeight-sHeight,swordYCoord);
+			//sword.setPosition(3*width/4+pHeight-sHeight,swordYCoord);
 
 			self.setFillColor(sf::Color(239, 48, 36));
+			self.setOutlineColor(sf::Color(0,0,0));
+			self.setOutlineThickness(1);
+
 			sword.setFillColor(sf::Color(111,111,111));
 		}
+		resetSword();
 		leaping = false;
 		swinging = false;
+		elapsedSwingTime = 0;
+	}
+
+	void resetSword()
+	{
+		float pHeight = selfBound().height;
+		float sHeight = swordBound().height;
+		int swordYCoord = selfBound().top-sHeight/2;
+		if ( firstPlayer )
+		{
+			sword.setPosition(selfBound().left,swordYCoord);
+		}
+		else 
+		{
+			sword.setPosition(selfBound().left+pHeight-sHeight,swordYCoord);
+		}
 	}
 
 	void act() {
@@ -74,7 +134,7 @@ class Player {
 		{
 			if ( firstPlayer )
 			{
-				if ( self.getPosition().x < width )
+				if ( (self.getPosition().x + selfBound().width) < width )
 				{
 					step = stepping*dt/1000;
 				}
@@ -88,7 +148,27 @@ class Player {
 			}
 		}
 		self.move(step,0);
-		sword.move(step,0);
+		if ( swinging )
+		{
+			elapsedSwingTime += dt;
+			double swingPercent = elapsedSwingTime / 250.0;
+			if ( firstPlayer )
+				sword.setPosition(selfBound().left+swingPercent*50, selfBound().top+swing(swingPercent));
+			else
+				sword.setPosition(selfBound().left-swingPercent*50, selfBound().top+swing(-1*swingPercent));
+			printf("%d %d %F\n", dt, swingPercent, swing(swingPercent));
+			if ( elapsedSwingTime > 250 )
+			{
+				swinging = false;
+				elapsedSwingTime = 0;
+				sword.setPosition(selfBound().left, selfBound().top);
+				printf("---\n");
+			}
+		}
+		else
+		{
+			sword.move(step,0);
+		}
 	}
 
 	void draw(sf::RenderWindow& w)
@@ -102,6 +182,11 @@ class Player {
 int main() {
 	sf::RenderWindow window(sf::VideoMode(width, height), "mini samurai");
 	sf::Clock clock;
+
+	sf::Texture back;
+	sf::Sprite background(back);
+	background.setTextureRect(sf::IntRect(0,0,width,height));
+	back.loadFromFile("drawing.png");
 
 	window.clear();
 
@@ -136,12 +221,15 @@ int main() {
 		}
 		s.tick(dt);
 		f.tick(dt);
+
+		// The universe's least-efficient drawing method
 		window.clear();
+		window.draw(background);
 
 		f.draw(window);
 		s.draw(window);
-		clock.restart();
 
+		clock.restart();
 		window.display();
 	}
 }
